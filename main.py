288b481d43b61f8834b2c2fa824a2682a118f8b6
Hatsune Miku <73402249+Weebed-Coder@users.noreply.github.com>
2023-07-11 15:41:36 -0400

    Added video object
    You can now create a video object, and it will automatically have the id, upload time, long URL and short URL (if initially provided) readily available from the newly created object.

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import requests, datetime
 
 class tiktok_utils:
+    class Video:
+        '''
+        A TikTok video
+        '''
+        url         = None
+        short_url   = None
+        id          = None
+        upload_time = None
+
+        def __init__(self, url) -> None:
+            if tiktok_utils.is_short_url(url): self.short_url = url
+            # TODO: Get short URL even if it is not initially provided.
+
+            self.url         = tiktok_utils.ensure_full_url(url)
+            self.id          = tiktok_utils.get_id(self.url)
+            self.upload_time = tiktok_utils.get_time(self.url)
+
     def get_id(full_url: str) -> int:
         '''
         "url" has be a full URL, not the short URL. 
         '''
-        data = full_url.split('/')
-        partial_id = data[-1] # partial_id is the "7252031033367956742?_r=1&_t=8dkjXJ3rFvu part of the url"
 
-        id = partial_id.split("?")[0] # removing everything after "?" to get actual id
+        data       = full_url.split('/')
+        partial_id = data[-1] # partial_id is the "7252031033367956742?_r=1&_t=8dkjXJ3rFvu part of the url"
+        id         = partial_id.split("?")[0] # removing everything after "?" to get actual id
+
         return int(id)
 
     def get_full_url(short_url: str) -> str:
@@ -17,24 +35,36 @@ class tiktok_utils:
         "short_url" should be a short URL, not the full URL.
         '''
         r = requests.get(short_url)
-        headers = r.history[0].headers # Gets the long URL from a redirect
+
+        headers  = r.history[0].headers # Gets the long URL from a redirect
         location = headers['location']
 
         return location
 
-    def get_time(url: str) -> datetime.datetime:
+    def is_short_url(url: str) -> bool:
         if 36 >= len(url):
-            url = tiktok_utils.get_full_url(url) # Converting it to a full URL if needed
+            return True
+        return False
+    
+    def ensure_full_url(url: str) -> str:
+        if tiktok_utils.is_short_url(url):
+            full_url = tiktok_utils.get_full_url(url)
+            return full_url
+        return url
+
+    def get_time(url: str) -> datetime.datetime:
+        if tiktok_utils.is_short_url(url):
+            url = tiktok_utils.get_full_url(url)
 
         id = tiktok_utils.get_id(url)
-        binary = bin(id)
-        binary_32 = binary[0:33] # Going to 33 to account for "0b"
-
+        binary    = bin(id)
+        binary_32 = binary[0:33]                               # Going to 33 to account for "0b"
         unix_time = int(binary_32, 2)
         date_time = datetime.datetime.fromtimestamp(unix_time)
+
         return date_time
 
 if __name__ == "__main__":
-    time = tiktok_utils.get_time("https://www.tiktok.com/t/ZT8eKsbgF/")
+    tiktok_video = tiktok_utils.Video("https://www.tiktok.com/t/ZT8eKsbgF/")
 
-    print(time)
+    for data in tiktok_video.__dict__: print(f"{data}: {tiktok_video.__dict__[data]}")
